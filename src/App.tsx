9a2a85b0e6135c01a063b14482850adacbb36d9f
harithzamri <harithtoikee96@gmail.com>
2021-04-29 09:55:55 +0800

    do essentials stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './pages/login';
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route component={Login} exact path="/login" />
+        </Switch>
+      </Router>
+    </>
+  );
 };
 
 export default App;
