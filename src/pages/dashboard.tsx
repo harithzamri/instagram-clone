d33fd5d078fed046002dcd22601751b466edb1f6
harithzamri <harithtoikee96@gmail.com>
2021-05-04 09:14:35 +0800

    adding dashboard pages

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface DashboardProps {
+  user: Record<string, string>;
+}
+
+function Dashboard({ user }: DashboardProps) {
+  return <div></div>;
+}
+
+export default Dashboard;
