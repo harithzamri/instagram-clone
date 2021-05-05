6c159e5a43db5bf81ebd65881aefd1fcb75981b4
harithzamri <harithtoikee96@gmail.com>
2021-05-05 09:30:34 +0800

    edit on dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Header from '../components/header';
 
 interface DashboardProps {
   user: Record<string, unknown>;
 }
 
 function Dashboard({ user }: DashboardProps) {
-  return <div className="bg-gray-background"></div>;
+  return (
+    <div className="bg-gray-background">
+      <Header />
+      <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg"></div>
+    </div>
+  );
 }
 
 export default Dashboard;
