c9f8a265e942e21cea80be5838b9e61ecaabd6d4
toikee <37389526+harithzamri@users.noreply.github.com>
2021-05-17 15:11:55 +0800

    edit on others

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+/** @format */
+
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
 
 function Sidebar() {
   return <div className="p-4"></div>;
